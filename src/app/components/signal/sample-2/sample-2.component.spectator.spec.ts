c629d442613432a48731ba146f0607ba91596645
sandranii <niyouu63@gmail.com>
2024-06-13 10:56:02 +0800

    add spectator in component with children & click

---
+++ src/app/components/signal/sample-2/sample-2.component.spectator.spec.ts
@@ -0,0 +1,50 @@
+import { Sample2Component } from './sample-2.component';
+import { Spectator, byTestId, createComponentFactory } from '@ngneat/spectator';
+import { UserListComponent } from './user-list/user-list.component';
+import { Sample1Component } from '../sample-1/sample-1.component';
+import { MockComponent } from 'ng-mocks';
+import { User } from '../../../interfaces';
+
+describe('Sample2Component with spectator', () => {
+  let spectator: Spectator<Sample2Component>;
+
+  let userList: UserListComponent | null;
+  let sample1: Sample1Component | null;
+
+  const createComponent = createComponentFactory({
+    component: Sample2Component,
+    shallow: true,
+    declarations: [
+      MockComponent(UserListComponent),
+      MockComponent(Sample1Component)
+    ]
+  })
+
+  beforeEach(() => {
+    spectator = createComponent();
+
+    userList = spectator.query(UserListComponent);
+    sample1 = spectator.query(Sample1Component);
+
+  })
+
+  it('傳 users 到 userList 子組件', () => {
+    console.log('userList?.userList',userList?.userList);
+    console.log('spectator.component.users', spectator.component.users);
+    expect(userList?.userList as any).toEqual(spectator.component.users);  // InputSignal 會有轉換型別問題，先用as any
+  })
+
+  it('點擊按鈕"Add Andy" 且 users 增加 Andy 的資料(加在第一組)', () => {
+    let userAndy: User = {
+      id: 5,
+      firstName: 'Andy',
+      lastName: 'Bernard',
+      username: 'andy.bernard',
+    };
+    expect(userList?.userList.length).toBe(4); 
+    spectator.click(byTestId('add-button')); // 點擊按鈕
+    expect(userList?.userList.length).toBe(5);
+    expect((userList?.userList as any)[0]).toEqual(userAndy);  // InputSignal 會有轉換型別問題，先用as any
+  })
+
+});
--- src/app/components/signal/sample-2/user-list/user-list.component.spectator.spec.ts
+++ src/app/components/signal/sample-2/user-list/user-list.component.spectator.spec.ts
@@ -1,5 +1,7 @@
+import { InputSignal, signal } from '@angular/core';
+import { ModifiedUser, User } from '../../../../interfaces';
 import { UserListComponent } from './user-list.component';
-import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import { Spectator, byTestId, createComponentFactory } from '@ngneat/spectator';
 
 describe('UserListComponent', () => {
   let spectator: Spectator<UserListComponent>;
@@ -18,9 +20,59 @@ describe('UserListComponent', () => {
   })
 
   // 因為有 userList = input.required，沒給會報錯
-  it('測試 spectator', () => {
+  xit('測試 spectator', () => {
     console.log(spectator.component.counter)
     expect(spectator.component.counter).toBe(5); 
+  })
+
+  // InputSignal<ModifiedUser[], User[]> 型別問題
+  xit('開始搜尋', () => {
+
+    const users: User[] = [
+      {
+        id: 1,
+        firstName: 'Michael',
+        lastName: 'Scott',
+        username: 'michael.scott',
+      },
+      {
+        id: 2,
+        firstName: 'Dwight',
+        lastName: 'Schrute',
+        username: 'dwight.schrute',
+      },
+      {
+        id: 3,
+        firstName: 'Angela',
+        lastName: 'Martin',
+        username: 'angela.martin',
+      },
+      {
+        id: 4,
+        firstName: 'Jim',
+        lastName: 'Halpert',
+        username: 'jim.halpert',
+      },
+    ];
+
+    // 將 User 陣列轉換成 ModifiedUser 陣列
+    const modifiedUsers = users.map(user => ({
+      ...user,
+      displayName: `${user.firstName} ${user.lastName}`,
+    }));
+
+    // 創建 InputSignal
+    // const userListInputSignal: InputSignal<ModifiedUser[], User[]> = signal(modifiedUsers);
+
+    // spectator.setInput('userList', userListInputSignal);
+    // spectator.setInput('userList', modifiedUsers);
+    // 模擬輸入查詢字串
+    const searchTerm = 'Jane';
+    spectator.typeInElement(searchTerm, byTestId('search-term-input'));
+     // 檢查過濾後的使用者清單
+     const filteredUsers = spectator.component.filteredUsers();
+     expect(filteredUsers.length).toBe(1);
+     expect(filteredUsers[0].displayName).toBe('Jane Smith');
   })
 
 });
